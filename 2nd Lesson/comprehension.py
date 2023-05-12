e1e5ea4697fefdd14b660c936a6648fb867f53f2
Silv <forenks1005@gmail.com>
2023-05-11 20:35:10 -0500

    Appending new files

---
+++ 2nd Lesson/comprehension.py
@@ -0,0 +1,4 @@
+lista = [i*2 for i in range(1, 57)]
+
+print(lista)
+
---
+++ 2nd Lesson/paradigm.py
@@ -0,0 +1,7 @@
+import math
+
+power = lambda x: x**2
+
+onelinecodefunction = lambda fisrtParameter, secondndparemeter: (fisrtParameter**2) + math.sqrt(secondndparemeter)
+
+print(onelinecodefunction(1, 64))
