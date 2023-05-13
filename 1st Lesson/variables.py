22c71c5bd5ee826c6e597ee56f83c1e0c4354d74
Silv <forenks1005@gmail.com>
2023-05-13 04:31:12 -0500

    appending Variables

---
+++ 1st Lesson/variables.py
@@ -0,0 +1,27 @@
+#int 
+integer = 15
+
+#float
+floatingPoint = 3.14
+
+#string
+string = "Hello World"
+
+#boolean
+itstrue = True
+itsfalse = False
+
+#List
+myList = [1, 2, 3, "Hi", False, ["omg", 1, 3]]
+print(myList)
+
+#Dictionary
+
+dictionary = {
+    "Ages" : [19, 34, 21]
+}
+print(dictionary)
+
+dictionary["names"] = ["Paul", "Silv", "bRUH"]
+
+print (dictionary)
