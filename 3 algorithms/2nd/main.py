03a885169a936085cb7cbdf8e33993965a11ff1d
Silv <forenks1005@gmail.com>
2023-05-14 13:35:29 -0500

    2nd almost done

---
+++ 3 algorithms/2nd/main.py
@@ -0,0 +1,44 @@
+
+multiplication = lambda x, y: x * y
+add = lambda x, y: x + y
+subtract = lambda x, y: x - y
+divide = lambda x, y: x / y
+
+operations = {"*": multiplication,
+               "+": add,
+               "-": subtract,
+               "/": divide
+               }
+
+
+def analizador(ecuacion: str):
+    equationSolved = ecuacion[:ecuacion.find("=") - 1].split(" ")
+    print(equationSolved)
+
+    for operation in operations.keys():
+        index = 0
+        while index < len(equationSolved):
+            if len(equationSolved) == 1:
+                break 
+
+            if equationSolved[index] == operation:
+                print(ecuacion)
+                
+                result = operations[operation](int(equationSolved[index - 1]), int(equationSolved[index + 1]))
+                element_to_be_replaced = " ".join(equationSolved[index - 1: index + 2])
+
+                ecuacion = ecuacion.replace(element_to_be_replaced, str(result))
+
+                equationSolved = ecuacion[:ecuacion.find("=") - 1].split(" ")
+                index = 0
+            else:
+                index += 1
+
+    print(ecuacion)
+
+
+def main():
+    ecuacion = input("Ingresa la ecuacion a resolver: ")
+    analizador(ecuacion)
+
+main()
