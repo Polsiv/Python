bcad65b1547f20a9e35d3f4b38660afc0610a2ff
Polsiv <forenks1005@gmail.com>
2023-06-10 00:01:05 -0500

    Appending new files

--- Practice/numpy/matrices.py
+++ Practice/numpy/matrices.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+#<-------------------Basic matrices------------------------>
+
 matrix = np.array([[1, 2], 
                   [3, 4]])
 print(matrix)
@@ -20,6 +22,10 @@ print(matrix)
 matrix *= 5
 print(f'Matrix multiplied by 5:{matrix}')
 
+
+
+#<-------------------Broadcasting------------------------>
+
 #product between vector and matric using broadcasting
 x = np.array([3, 2])
 
@@ -33,4 +39,20 @@ x2 = np.array([6, 7, 8, 9])
 print(f'Matrix 2 by x2: {x2.reshape(2, 2) * matrix2}')
 
 
+#<-------------------Multiplying matrices------------------------->
 
+matrix3 = np.array([[10, 20],
+                    [30, 40]])
+
+matrix4 = np.array([[2, 4],
+                    [6, 8]])
+print(f'Matrix3 times matrix4 is equal to: {matrix3 * matrix4}')
+
+#<-------------------Multiplying matrices (cross)------------------------->
+
+print(f'Matrix3 times matrix4 (cross product) is equal to: {matrix3.dot(matrix4)}')
+
+#<-------------------Transpose matrix------------------------->
+
+
+print(f'Original matrix:\n{matrix3} \n Tranpose Matrix:\n {matrix3.T}')
