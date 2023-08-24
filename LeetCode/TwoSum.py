17c52c4424be82d1f82afa570db63593c9c7d4c9
Silv <forenks1005@gmail.com>
2023-08-24 04:32:35 +0200

    .

---
+++ LeetCode/TwoSum.py
@@ -0,0 +1,10 @@
+lass Solution(object):
+    def twoSum(self, nums, target):
+      prevMap = {} #val : index
+
+      for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return[prevMap[diff], i]
+            prevMap[n] = i
+        
