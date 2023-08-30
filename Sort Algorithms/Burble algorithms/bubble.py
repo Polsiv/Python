f9d5e1132350334062b9593b8b329c2a8b4f5cd2
Silv <forenks1005@gmail.com>
2023-08-30 03:46:59 +0200

    Removing files

--- Sort Algorithms/Burble algorithms/bubble.py
+++
@@ -1,9 +0,0 @@
-def bubble(list):
-    for i in range(0, len(list)-1):
-        for j in range(0, len(list)-i-1):
-            if (list[j] > list[j + 1]):
-                list[j], list[j + 1] = list[j+1], list[j]
-                print(list)
-
-list = [51, 7, 33, 13, 79, 21]
-bubble(list)
