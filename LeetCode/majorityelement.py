59e1c43f5aea27498c1e480ff3281a0ae45f293d
Silv <forenks1005@gmail.com>
2023-09-05 07:49:48 -0500

    Appending new py files

---
+++ LeetCode/majorityelement.py
@@ -0,0 +1,24 @@
+def findmajority(list):
+
+    frequencies = {}
+
+    for i in list:
+        frequencies[i] = 0
+
+
+
+
+    
+    print(frequencies)
+
+
+
+
+
+
+
+list = [1, 2, 4, 5, 1, 2, 4, 1, 1, 1]
+
+
+findmajority(list)
+
