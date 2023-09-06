4b3ea226b2f14dff2a7ab1aafbdb543faec6d728
Silv <forenks1005@gmail.com>
2023-09-06 00:08:29 -0500

    Appending new files

---
+++ Data Structures/TDA/static&dinamicvariables.py
@@ -0,0 +1,19 @@
+print("Static variables=============")
+a = 10
+b = a
+
+print(id(b))
+print(id(a))
+
+print("====")
+
+b = 15
+
+print(id(b))
+print(id(a))
+
+print(a, b)
+
+print("Dynamic variables=============")
+
+
--- LeetCode/majorityelement.py
+++ LeetCode/majorityelement.py
@@ -3,7 +3,7 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         frequencies = {}
         count = 0
-        for i in :
+        for i in nums:
             frequencies[i] = frequencies.get(i, 0) + 1
 
         return max(frequencies, key=frequencies.get)
