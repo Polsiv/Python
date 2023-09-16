95bbcb94399af0953f7e38be18930d18a3e1285b
Silv <forenks1005@gmail.com>
2023-09-16 01:04:34 -0500

    Appending files

---
+++ LeetCode/longestcommonprefix.py
@@ -0,0 +1,11 @@
+#https://www.youtube.com/watch?v=0sWShKIJoo4 Explanation
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ""
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return res
+            res += strs[0][i]
+        return res
