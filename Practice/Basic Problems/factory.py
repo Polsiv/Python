1a13d284052666e3d93cc73208b3aec490b30295
Silv <forenks1005@gmail.com>
2023-10-24 19:34:22 -0500

    removing

--- Practice/Basic Problems/factory.py
+++
@@ -1,8 +0,0 @@
-angle = 0
-
-float(input("Enter the angle"))
-
-if(angle == 90):
-    print("Right angle!")
-else:
-    print("Not a right angle")
--- Practice/numpy/matrixproblem.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-coefficent = np.array([[2, 1, -3],
-                       [5 , -4, 1],
-                       [1, -1, -4]])
-
-solution = np.array([7, -19, 4])
-
-print(np.linalg.solve(coefficent, solution))
