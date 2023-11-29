320e1a3eb20413b8cd9e2da1fc0f786ae89af68e
Silv <forenks1005@gmail.com>
2023-11-29 17:50:14 -0500

    updating

--- Streamlit/test.py
+++ Streamlit/test.py
@@ -41,7 +41,7 @@ st.table(dataframe)
 
 
 # map_data = pd.DataFrame(
-#     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],    s
+#     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],    
 #     columns=['lat', 'lon'])
 
 # st.map(map_data)
