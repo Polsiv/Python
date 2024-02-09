8b9aac6d08641903037442f4ddb14d06ad8aae7a
Silv <forenks1005@gmail.com>
2024-02-09 09:40:51 -0500

    just bullshit

---
+++ Elective Programming/main.py
@@ -0,0 +1,19 @@
+space_odyssey = 2001
+print(space_odyssey)
+print(type("Good night & Good luck"))
+print(type(True))
+result = 10 * 3.0
+print(result)
+
+x =input(int("enter the number 7, is no gay"))
+y= input(int("enter the number 4, is no gay"))
+
+print(x + y)
+print(x - y)
+print(x * y)
+try:
+    x / y
+except:
+    print("DIVISIOJ POR 0 MI BRO")
+
+print(x/y)
