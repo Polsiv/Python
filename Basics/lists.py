8b7402c18f896124b75d9491c009c8c66ba34549
Silv <forenks1005@gmail.com>
2024-02-16 08:44:21 -0500

    appending

--- Basics/lists.py
+++ Basics/lists.py
@@ -32,3 +32,23 @@ temp = fruits[0]
 fruits[0] = fruits[2]
 fruits[2] = temp
 print(fruits)
+
+
+print(list("ELPEPE"))
+
+#turns string into a list   
+
+mylist.insert(1,"fruio")
+
+#enters value in specific index
+
+mylist.extend("EL PEPE")
+#turns the string into a list and appends it to the list
+
+#del mylist[3] removes the index
+
+copy_list = mylist.copy()
+#creates an original copy of the list
+
+#mylist.pop(index) removes the index
+#my list.clear() clears the entire fucking list
