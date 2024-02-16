14b7b7a7ab9c9d6eb246aa1d2a5d07575aec89b9
Silv <forenks1005@gmail.com>
2024-02-15 23:53:58 -0500

    ¨appending

--- Manim/parametriccurve.py
+++ Manim/parametriccurve.py
@@ -2,4 +2,7 @@ from manim import *
 
 class Parametric(Scene):
     def construct (self):
-        
+        plane = NumberPlane(x_range=[-8, 8, 1], x_length=15, y_range=[-8, 8, 1], y_length=15).shift(DOWN*3)
+
+        self.play(Create(plane), rate_func = smoothstep)
+
