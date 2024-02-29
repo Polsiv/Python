7bbc8c143b0c25a4334c1ad894dff683a3fdec23
Silv <forenks1005@gmail.com>
2024-02-29 00:41:22 -0500

    APPEND

--- Medium/Elective Programming/baseCalculator.py
+++ Medium/Elective Programming/baseCalculator.py
@@ -4,11 +4,9 @@ def base_to_base():
     return 0
 
 def decimal_to_base(number, base):
-    above_decimal ="ABCDEF"
-    coc = 1
+    above_decimal, coc, result ="ABCDEF", 1, ""
     float_part = float("{:.2f}".format(number % 1))
     int_part = int(number // 1)
-    result = ""
 
    #converting the int_part
     while coc != 0:
@@ -28,17 +26,34 @@ def decimal_to_base(number, base):
     
     return result[::-1] + "." + float_part_complete
 
+def base_to_decimal(number, base):
+    above_decimal = {"A": 10, "B":  11, "C": 12, "D": 13, "E": 14, "F": 15}
+    
+    for i in range (len(number)):
+        if number[i] in above_decimal.keys(): number = number.replace(str(number[i]), str(above_decimal[number[i]]))
+    
+    
+    int_part, float_part = number.split(".")
 
-def base_to_decimal():
     return 0
 
-option = int(input(f'==Welcome to the number base calculator, enter the option you desire!== \n 1) From base N to base M. \n 2) From base Decimal to N. \n 3) From base N to Decimal. \n {"="*30} \n Enter your option:'))
 
 
-if option == 1:base_to_base()
-if option == 2:
-    number = float(input("Enter the decimal number to covert: "))
-    base = int(input("Enter the target base: "))
-    
-    print(f'the result is: {decimal_to_base(number, base)}')
-if option == 3: base_to_decimal()
+def main():
+
+    option = int(input(f'==Welcome to the number base calculator, enter the option you desire!== \n 1) From base N to base M. \n 2) From base Decimal to N. \n 3) From base N to Decimal. \n {"="*30} \n Enter your option:'))
+    if option == 1:
+        print("xd")
+
+    if option == 2:
+        number = float(input("Enter the decimal number to covert: "))
+        base = int(input("Enter the target base: "))
+        print(f'Your number {number} is written as: {decimal_to_base(number, base)} in {base} base.')
+    if option == 3: 
+        base = int(input("Enter the base: "))
+        number = (input("Enter the number to covert to decimal: "))
+        base_to_decimal(number, base)
+        # print(f'Your number {number} is written as: {base(number, base)} in decimal.')
+
+
+main()
