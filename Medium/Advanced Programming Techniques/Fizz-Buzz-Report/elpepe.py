5353b77a645f9a1c2a2be8ed0d838838326e1199
Silv <forenks1005@gmail.com>
2024-03-09 17:52:46 -0500

    app

--- Medium/Advanced Programming Techniques/Fizz-Buzz-Report/elpepe.py
+++
@@ -1,9 +0,0 @@
-class ManageInput:
-    def __init__(self, file_name):
-        self.__filename = file_name
-
-    def __read_file():
-        return 0
-    
-    def __remove_number():
-        return 0
