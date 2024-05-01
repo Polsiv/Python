9e64f8eba55af10fe80f99b72eab9b55cefd99c7
polsiv <forenks1005@gmail.com>
2024-05-01 09:42:20 -0500

    appending

--- Medium/Advanced Programming Techniques/FizzBuzzWeb/main.py
+++ Medium/Advanced Programming Techniques/FizzBuzzWeb/main.py
@@ -10,21 +10,29 @@ my_app = MyApp()
 @app.route('/numbers/<number>', methods = ['GET', 'POST', 'DELETE'])
 def numbers(number):
     """this is where we will get our data from Postman"""
-    if request.method == "GET":
-        return my_app.get_number(number)
+    try:
+        if request.method == "GET":
+            return my_app.get_number(int(number))
 
-    if request.method == "POST":
-        return my_app.post_number(int(number))
+        if request.method == "POST":
+            return my_app.post_number(int(number))
 
-    if request.method == "DELETE":
-        return my_app.delete_number(number)
+        if request.method == "DELETE":
+            return my_app.delete_number(int(number))
+
+    except ValueError:
+        return "Invalid input.", 400
 
 @app.route('/range/', methods = ['POST'])
 def get_range():
     """this is where we will get our data from Postman"""
-    limits = request.get_json()
-    low_limit = limits["lowlimit"]
-    sup_limit = limits["suplimit"]
-    return my_app.get_range(low_limit, sup_limit)
+    try:
+        limits = request.get_json()
+        low_limit = limits["lowlimit"]
+        sup_limit = limits["suplimit"]
+        return my_app.get_range(int(low_limit), int(sup_limit))
+
+    except ValueError:
+        return "Invalid input.", 400
 
 app.run(host = "0.0.0.0", port = 80)
