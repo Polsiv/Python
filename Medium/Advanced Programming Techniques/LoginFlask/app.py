ced8a5a034ba3eca96e2aa9538dd9d41b0bb75a0
polsiv <forenks1005@gmail.com>
2024-05-23 19:56:31 -0500

    test

---
+++ Medium/Advanced Programming Techniques/LoginFlask/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+
+class User(db.model, UserMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), nullable = False)
+    password = db.Column(db.String(80), nullable = False)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'secretkey'
+
+@app.route('/')
+def home():
+    return render_template("home.html")    
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+if __name__ == "__main__":
+    app.run(debug= True )
