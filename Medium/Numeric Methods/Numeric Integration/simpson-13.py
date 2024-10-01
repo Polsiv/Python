3939e65ddd1ca820cc696a9f93979f985bad31b0
Silv <forenks1005@gmail.com>
2024-10-01 13:32:10 -0500

    lol

---
+++ Medium/Numeric Methods/Numeric Integration/simpson-13.py
@@ -0,0 +1,22 @@
+from numpy import linspace
+
+
+
+def f(x):
+    return x
+
+
+
+def simpson(a, b, n):
+
+    dx = (b - a)/n
+    subsections = linspace(a, b, n + 1 )
+
+
+    pass
+
+
+a = 0
+b = 0
+
+n = 2
