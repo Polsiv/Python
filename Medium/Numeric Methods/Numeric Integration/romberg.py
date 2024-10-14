de21216f3de7e3e1db4b037533f477bde1064215
Silv <forenks1005@gmail.com>
2024-10-14 14:54:49 -0500

    app

--- Medium/Numeric Methods/Numeric Integration/romberg.py
+++ Medium/Numeric Methods/Numeric Integration/romberg.py
@@ -1,46 +1,32 @@
-
 import numpy as np
 
 def f(x):
-    return x**2
+    return x ** 2
 
 def trapezoid(a, b, n):
     x = np.linspace(a, b, n + 1)
-    dx=(b-a)
-    A=0
-    for i in range(1,len(x)-1):
-        A+=(2*f(x[i]))
+    dx = (b - a) / n  
+    A = 0
+    for i in range(1, len(x) - 1):
+        A += 2 * f(x[i])
     
-    I=dx*(f(x[0])+A+f(x[n]))/(2*n)
+    I = dx * (f(x[0]) + A + f(x[-1])) / 2  
     return I
 
-
-
 def romberg(a, b, n):
-    
-    col_act = 0
     matrix = np.zeros((n, n))
 
-    for i in range(0, n):
-        for j in range(0, n):
+    for i in range(n):
+        matrix[i, 0] = trapezoid(a, b, 2 ** i)
 
-            if i == 0:
-                matrix[j][i] = j
-            
-            elif i == 1:
-                matrix[j][i] = trapezoid(a, b, 2 ** j)
-
-            else:
-                try:
-                    matrix[j + 1][i] = (1 / (4 ** i - 1)) * (((4 ** i) * matrix[i][j - 1]) - matrix[i - 1][j - 1]) 
-                except:
-                    pass
+    for i in range(1, n):
+        for j in range(i, n):
+            matrix[j, i] = (4 ** i * matrix[j, i - 1] - matrix[j - 1, i -  1]) / (4 ** i - 1)
 
     return matrix
-
 
 a = 0
 b = 3
 n = 3
 
-print(romberg(a, b, n))   
+print(romberg(a, b, n))
