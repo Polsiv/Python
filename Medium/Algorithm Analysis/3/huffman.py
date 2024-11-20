298d2557ee9a207e1fd7ac2e1100ca15d5674e7b
polsiv <forenks1005@gmail.com>
2024-11-19 19:05:33 -0500

    from main

--- Medium/Algorithm Analysis/3/huffman.py
+++ Medium/Algorithm Analysis/3/huffman.py
@@ -1,73 +1,63 @@
+import heapq
+
 class Node:
     def __init__(self, char, freq):
         self.char = char
         self.freq = freq
         self.left = None
         self.right = None
-        
+
+    # for priority queue (frecuency basically)
+    def __lt__(self, other):
+        return self.freq < other.freq
+
+def huffman(frequencies):
+    # Priority queue (min-heap)
+    heap = [Node(char, freq) for char, freq in frequencies.items()]
+    heapq.heapify(heap)
     
-class Huffman:
-    def __init__(self, freq) -> None:
-        self.freq = freq
-        self.codes = {}
-        self.root = None
-        
-    def tree(self):
-        nodes = [Node(char, freq) for char, freq in self.freq.items()]
-        
-        
-        while len(nodes) > 1:
-            nodes.sort(key =  lambda x: x.freq)
-            
-            left = nodes.pop(0)
-            right = nodes.pop(0)
-            
-            merged = Node(None, left.freq + right.freq)
-            
-            merged.left = left
-            merged. right = right
-            
-            nodes.append(merged)
-            
-        self.root = nodes[0]
-        
-        
-    def generate_codes(self, node = None, current_node = ""):
+    while len(heap) > 1:
+        # get 2 nodes with the smallest frequencies
+        left = heapq.heappop(heap)
+        right = heapq.heappop(heap)
+	
+    	# new node, the sum of the 2 nodes selected
+        merged = Node(None, left.freq + right.freq)
+        merged.left = left
+        merged.right = right
+
+        # new node, so append it into the heap
+        heapq.heappush(heap, merged)
+
+    # final node = root of the huffman tree
+    root = heap[0]
+
+    # generating the codes
+    
+    codes = {}
+    def generate_codes(node, current_code = ""):
         if node is None:
-            node = self.root
-            
-        if node.char is not None:
-            self.codes[node.char] = current_node
             return
-        
-        self.generate_codes(node.left, current_node + "0")
-        self.generate_codes(node.right, current_node + "1")
-        
-        
-    def get_codes(self):
-        if not self.codes:
-            self.generate_codes()
-        return self.codes
-    
-    
-    
+        if node.char is not None:  # leaf node
+            codes[node.char] = current_code
+            return
+        generate_codes(node.left, current_code + "0")
+        generate_codes(node.right, current_code + "1")
+
+    generate_codes(root)
+    return codes
 def main():
-    
-    freq = {
-        'h': 20,
+    frequencies = {
         'e': 20,
         'o': 20,
+        'h': 20,
         'l': 40,
     }
     
-    huffman = Huffman(freq)
-    huffman.tree()
-    codes = huffman.get_codes()
-    
+    codes = huffman(frequencies)
+
     for i, j in codes.items():
+        
         print(f"{i}: {j}")
-        
-        
-        
+
 main()
-    
